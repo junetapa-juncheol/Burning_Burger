bdd2b0d8f21d86f08bf842971ca77d9eabf3e46d
junetapa-juncheol <jun22sky@gmail.com>
2025-06-27 10:47:26 +0900

    save

--- js/main.js
+++ js/main.js
@@ -120,13 +120,13 @@ class App {
         document.documentElement.setAttribute('data-theme', currentTheme);
 
         if (themeToggle) {
-            const icon = themeToggle.querySelector('i');
-            if (currentTheme === 'dark') {
-                this.utils.removeClass(icon, 'fa-moon');
-                this.utils.addClass(icon, 'fa-sun');
-            } else {
-                this.utils.removeClass(icon, 'fa-sun');
-                this.utils.addClass(icon, 'fa-moon');
+            const svg = themeToggle.querySelector('svg');
+            if (svg && currentTheme === 'dark') {
+                // Sun icon for dark theme
+                svg.innerHTML = '<path fill="currentColor" d="M361.5 1.2c5 1.3 8.3 6.5 6.9 11.4L343.4 82.7l28.3-12.6c5-2.2 10.9 0 13.1 5s0 10.9-5 13.1l-28.3 12.6 69.1 25.4c5 1.8 7.6 7.4 5.8 12.4s-7.4 7.6-12.4 5.8L344 118l12.6 28.3c2.2 5 0 10.9-5 13.1s-10.9 0-13.1-5L326.9 126l-25.4 69.1c-1.8 5-7.4 7.6-12.4 5.8s-7.6-7.4-5.8-12.4L310.7 119l-28.3 12.6c-5 2.2-10.9 0-13.1-5s0-10.9 5-13.1l28.3-12.6L233.5 75.5c-5-1.8-7.6-7.4-5.8-12.4s7.4-7.6 12.4-5.8L309.2 83l-12.6-28.3c-2.2-5 0-10.9 5-13.1s10.9 0 13.1 5L327.3 75l25.4-69.1c1.8-5 7.4-7.6 12.4-5.8zM287.6 64.7c-1.2 0-2.4.1-3.6.2L265.5 33.2c-1.7-3.2-5.2-5.2-9-5.2s-7.2 2-9 5.2L229 65c-1.2-.1-2.4-.2-3.6-.2c-15.5 0-29.3 7.2-38.4 18.5L148 64.8c-3.2-1.7-7.2-1.7-10.4 0s-5.4 4.7-5.4 8.2V96c0 8.8 7.2 16 16 16h128c8.8 0 16-7.2 16-16V73c0-3.5-1.9-6.7-5-8.4z"/>';
+            } else if (svg) {
+                // Moon icon for light theme
+                svg.innerHTML = '<path fill="currentColor" d="m223.5 32C100 32 0 132.3 0 256S100 480 223.5 480c60.6 0 115.5-24.2 155.8-63.4c5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6c-96.9 0-175.5-78.8-175.5-176 0-65.8 36-123.1 89.3-153.3c6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z"/>';
             }
             
             this.utils.on(themeToggle, 'click', () => this.toggleTheme());
@@ -142,13 +142,15 @@ class App {
         
         const themeToggle = this.utils.$('.theme-toggle');
         if (themeToggle) {
-            const icon = themeToggle.querySelector('i');
-            if (newTheme === 'dark') {
-                this.utils.removeClass(icon, 'fa-moon');
-                this.utils.addClass(icon, 'fa-sun');
-            } else {
-                this.utils.removeClass(icon, 'fa-sun');
-                this.utils.addClass(icon, 'fa-moon');
+            const svg = themeToggle.querySelector('svg');
+            if (svg) {
+                if (newTheme === 'dark') {
+                    // Sun icon for dark theme
+                    svg.innerHTML = '<path fill="currentColor" d="M361.5 1.2c5 1.3 8.3 6.5 6.9 11.4L343.4 82.7l28.3-12.6c5-2.2 10.9 0 13.1 5s0 10.9-5 13.1l-28.3 12.6 69.1 25.4c5 1.8 7.6 7.4 5.8 12.4s-7.4 7.6-12.4 5.8L344 118l12.6 28.3c2.2 5 0 10.9-5 13.1s-10.9 0-13.1-5L326.9 126l-25.4 69.1c-1.8 5-7.4 7.6-12.4 5.8s-7.6-7.4-5.8-12.4L310.7 119l-28.3 12.6c-5 2.2-10.9 0-13.1-5s0-10.9 5-13.1l28.3-12.6L233.5 75.5c-5-1.8-7.6-7.4-5.8-12.4s7.4-7.6 12.4-5.8L309.2 83l-12.6-28.3c-2.2-5 0-10.9 5-13.1s10.9 0 13.1 5L327.3 75l25.4-69.1c1.8-5 7.4-7.6 12.4-5.8z"/>';
+                } else {
+                    // Moon icon for light theme
+                    svg.innerHTML = '<path fill="currentColor" d="m223.5 32C100 32 0 132.3 0 256S100 480 223.5 480c60.6 0 115.5-24.2 155.8-63.4c5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6c-96.9 0-175.5-78.8-175.5-176 0-65.8 36-123.1 89.3-153.3c6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z"/>';
+                }
             }
         }
     }
