c631ad42461eaf4cb6fd4dd77ab5fc22fb6a2d41
junetapa-juncheol <jun22sky@gmail.com>
2025-07-01 12:52:13 +0900

    save

--- js/components/musicplayer.js
+++ js/components/musicplayer.js
@@ -519,17 +519,22 @@ class MusicPlayer {
     updateVolumeIcon() {
         if (!this.elements.volumeBtn) return;
         
-        const icon = this.utils.$('i', this.elements.volumeBtn);
-        if (!icon) return;
-        
-        icon.className = '';
+        const svg = this.utils.$('.icon-svg', this.elements.volumeBtn);
+        const path = this.utils.$('.icon-svg path', this.elements.volumeBtn);
+        if (!svg || !path) return;
         
         if (this.state.isMuted || this.state.volume === 0) {
-            icon.className = 'fas fa-volume-mute';
+            // 음소거 아이콘
+            svg.setAttribute('viewBox', '0 0 576 512');
+            path.setAttribute('d', 'M301.1 34.8C312.6 40 320 51.4 320 64V448c0 12.6-7.4 24-18.9 29.2s-25 3.1-34.4-5.3L131.8 352H64c-35.3 0-64-28.7-64-64V224c0-35.3 28.7-64 64-64h67.8L266.7 40.1c9.4-8.4 22.9-10.4 34.4-5.3zM425 167l55 55 55-55c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-55 55 55 55c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-55-55-55 55c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l55-55-55-55c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0z');
         } else if (this.state.volume < 0.5) {
-            icon.className = 'fas fa-volume-down';
+            // 낮은 볼륨 아이콘
+            svg.setAttribute('viewBox', '0 0 448 512');
+            path.setAttribute('d', 'M301.1 34.8C312.6 40 320 51.4 320 64V448c0 12.6-7.4 24-18.9 29.2s-25 3.1-34.4-5.3L131.8 352H64c-35.3 0-64-28.7-64-64V224c0-35.3 28.7-64 64-64h67.8L266.7 40.1c9.4-8.4 22.9-10.4 34.4-5.3zM412.6 181.5C434.1 199.1 448 225.9 448 256s-13.9 56.9-35.4 74.5c-10.3 8.4-25.4 6.8-33.8-3.5s-6.8-25.4 3.5-33.8C393.1 284.4 400 271 400 256s-6.9-28.4-17.7-37.3c-10.3-8.4-11.8-23.5-3.5-33.8s23.5-11.8 33.8-3.5z');
         } else {
-            icon.className = 'fas fa-volume-up';
+            // 높은 볼륨 아이콘 (원래 아이콘 유지)
+            svg.setAttribute('viewBox', '0 0 640 512');
+            path.setAttribute('d', 'M533.6 32.5C598.5 85.3 640 165.8 640 256s-41.5 170.8-106.4 223.5c-10.3 8.4-25.4 6.8-33.8-3.5s-6.8-25.4 3.5-33.8C557.5 398.2 592 329.2 592 256s-34.5-142.2-88.7-186.3c-10.3-8.4-11.8-23.5-3.5-33.8s23.5-11.8 33.8-3.5zM473.1 107c43.2 35.2 70.9 88.9 70.9 149s-27.7 113.8-70.9 149c-10.3 8.4-25.4 6.8-33.8-3.5s-6.8-25.4 3.5-33.8C475.3 341.3 496 301.1 496 256s-20.7-85.3-53.2-111.8c-10.3-8.4-11.8-23.5-3.5-33.8s23.5-11.8 33.8-3.5zm-60.5 74.5C434.1 199.1 448 225.9 448 256s-13.9 56.9-35.4 74.5c-10.3 8.4-25.4 6.8-33.8-3.5s-6.8-25.4 3.5-33.8C393.1 284.4 400 271 400 256s-6.9-28.4-17.7-37.3c-10.3-8.4-11.8-23.5-3.5-33.8s23.5-11.8 33.8-3.5zM301.1 34.8C312.6 40 320 51.4 320 64V448c0 12.6-7.4 24-18.9 29.2s-25 3.1-34.4-5.3L131.8 352H64c-35.3 0-64-28.7-64-64V224c0-35.3 28.7-64 64-64h67.8L266.7 40.1c9.4-8.4 22.9-10.4 34.4-5.3z');
         }
     }
     
@@ -615,9 +620,18 @@ class MusicPlayer {
     updateUI() {
         // 플레이/정지 버튼 아이콘
         if (this.elements.playPauseBtn) {
-            const icon = this.utils.$('i', this.elements.playPauseBtn);
-            if (icon) {
-                icon.className = this.state.isPlaying ? 'fas fa-pause' : 'fas fa-play';
+            const svg = this.utils.$('.play-pause-icon', this.elements.playPauseBtn);
+            const path = this.utils.$('.play-pause-icon path', this.elements.playPauseBtn);
+            if (svg && path) {
+                if (this.state.isPlaying) {
+                    // 일시정지 아이콘 (두 개의 세로 막대)
+                    svg.setAttribute('viewBox', '0 0 320 512');
+                    path.setAttribute('d', 'M48 64C21.5 64 0 85.5 0 112V400c0 26.5 21.5 48 48 48s48-21.5 48-48V112c0-26.5-21.5-48-48-48zm192 0c-26.5 0-48 21.5-48 48V400c0 26.5 21.5 48 48 48s48-21.5 48-48V112c0-26.5-21.5-48-48-48z');
+                } else {
+                    // 재생 아이콘 (삼각형)
+                    svg.setAttribute('viewBox', '0 0 384 512');
+                    path.setAttribute('d', 'M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z');
+                }
             }
         }
         
